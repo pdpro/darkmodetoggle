11bf886dafa8590243f5402383ec41dbe22a055d
pdpro <pdprolancer@gmail.com>
2020-06-08 22:30:14 +0530

    first commit

---
+++ precache-manifest.fdcd1e075568816efd230b73b6d6ab32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c56a761b46c15f618898203171c25ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b5f5d20fd617df51e0b",
+    "url": "/static/js/2.711d15c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.711d15c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9568daa534282f000579",
+    "url": "/static/js/main.cd785494.chunk.js"
+  },
+  {
+    "revision": "3dcdef9d7399862cb335",
+    "url": "/static/js/runtime-main.1e181b31.js"
+  }
+]);
